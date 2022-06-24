125faff59751a1375362d6c0dbe17059a90f2150
John Doe <johndoe@example.com>
2022-06-23 22:51:55 -0500

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import '../../App.scss';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3 mb-4">
+      <div className="container">
+        <Link to="/" className="navbar-brand fs-3 ubuntu">
+          <img
+            style={{ maxWidth: '200px' }}
+            className="float-left"
+            src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Rick_and_Morty.svg"
+            alt=""
+          />
+          <span className="text-primary"> App</span>
+        </Link>
+        <style jsx>{`
+          button[aria-expanded='false'] > .close {
+            display: none;
+          }
+          button[aria-expanded='true'] > .open {
+            display: none;
+          }
+        `}</style>
+        <button
+          className="navbar-toggler border-0"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+          <span class="fas fa-bars open text-dark"></span>
+          <span class="fas fa-times close text-dark"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNavAltMarkup"
+        >
+          <div className="navbar-nav fs-5">
+            <NavLink to="/" className="nav-link">
+              Characters
+            </NavLink>
+            <NavLink to="/episodes" className="nav-link">
+              Episode
+            </NavLink>
+            <NavLink
+              activeClassName="active"
+              className="nav-link"
+              to="/location"
+            >
+              Location
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
